cc75bd2c1e430fadd9983d982b29393ac96450be
zhangzhiqiang <zhangzhiqiang@jzic.com>
2019-12-03 01:28:10 +0800

    更改文件路径

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ var url=require('url');
 var fs=require('fs');
 var mine=require('./mine').types;//
 var path=require('path');
- 
+
 var server = http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
-    var realPath = path.join("/", pathname);    //这里设置自己的文件名称;
- 
+    var realPath = path.join("webapp", pathname);    //这里设置自己的文件名称;
+
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : 'unknown';
     fs.exists(realPath, function (exists) {
+        console.log('realPath',realPath)
+        console.log('exists',exists)
         if (!exists) {
             response.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
- 
+
             response.write("This request URL " + pathname + " was not found on this server.");
             response.end();
         } else {
